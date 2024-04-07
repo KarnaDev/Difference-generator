0a3e8f8b09223d2bb6e61efd1d5dafebb23dd700
polina karna <polina.karna@gmail.com>
2024-04-07 14:11:24 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -23,14 +23,12 @@ const compareFiles = (file1, file2) => {
   const diffs = uniqKeys.flatMap((key) => {
     const hasKeyInFile1 = _.has(file1, key);
     const hasKeyInFile2 = _.has(file2, key);
+    const valuesEqual = _.isEqual(file1[key], file2[key]);
 
-    if (hasKeyInFile1 && hasKeyInFile2) {
-      const valuesEqual = _.isEqual(file1[key], file2[key]);
-      if (valuesEqual) {
-        return [`  ${key}: ${file1[key]}`];
-      } else {
-        return [`- ${key}: ${file1[key]}`, `+ ${key}: ${file2[key]}`];
-      }
+    if (hasKeyInFile1 && hasKeyInFile2 && valuesEqual) {
+      return [`  ${key}: ${file1[key]}`];
+    } else if (hasKeyInFile1 && hasKeyInFile2 && !valuesEqual) {
+      return [`- ${key}: ${file1[key]}`, `+ ${key}: ${file2[key]}`];
     } else if (hasKeyInFile1) {
       return [`- ${key}: ${file1[key]}`];
     } else if (hasKeyInFile2) {
