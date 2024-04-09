31dda41c845d0d485ab51db9f5bde731c4a6f7c9
polina karna <polina.karna@gmail.com>
2024-04-09 22:18:43 +0300

    parsing moved to parser.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-import fs from 'fs';
 import _ from 'lodash';
-import path from 'path';
+import readFile from './parsers.js';
 
 // const getAbsolutePath = (file) => path.resolve(process.cwd(), file);
 // If, after processing given path, an absolute path has not yet been generated,
 // the current working directory is used
-const getAbsolutePath = (file) => path.resolve(file);
-
-const readFileAsJSON = (filePath) => {
-  const absPath = getAbsolutePath(filePath);
-  return JSON.parse(fs.readFileSync(absPath));
-};
 
 const getUniqKeys = (file1, file2) => {
   const keys1 = _.keys(file1);
   const keys2 = _.keys(file2);
   return _.union(keys1, keys2).sort();
 };
-
 const compareFiles = (file1, file2) => {
   const uniqKeys = getUniqKeys(file1, file2);
   const diffs = [];
@@ -41,8 +33,8 @@ const compareFiles = (file1, file2) => {
 };
 
 const genDiff = (filePath1, filePath2) => {
-  const file1 = readFileAsJSON(filePath1);
-  const file2 = readFileAsJSON(filePath2);
+  const file1 = readFile(filePath1);
+  const file2 = readFile(filePath2);
   const diffs = compareFiles(file1, file2);
   return `{\n  ${diffs.join('\n  ')}\n}`;
 };
