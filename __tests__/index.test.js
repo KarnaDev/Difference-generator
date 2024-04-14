611473dbc2a916a0022099c74d081e4d3311fbec
polina karna <polina.karna@gmail.com>
2024-04-14 22:36:59 +0300

    updated tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,15 +9,39 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('gendiff main flow for json', () => {
+test('gendiff non-specified formatter (stylish as default) for json', () => {
   const actual = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expected = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
+  const expected = fs.readFileSync(getFixturePath('result-stylish.txt'), 'utf-8');
   expect(actual).toEqual(expected);
 });
 
-test('gendiff main flow for yml', () => {
+test('gendiff non-specified formatter (stylish as default) for yml', () => {
   const actual = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
-  const expected = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
+  const expected = fs.readFileSync(getFixturePath('result-stylish.txt'), 'utf-8');
+  expect(actual).toEqual(expected);
+});
+
+test('gendiff stylish formatter for json', () => {
+  const actual = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
+  const expected = fs.readFileSync(getFixturePath('result-stylish.txt'), 'utf-8');
+  expect(actual).toEqual(expected);
+});
+
+test('gendiff stylish formatter for yml', () => {
+  const actual = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'), 'stylish');
+  const expected = fs.readFileSync(getFixturePath('result-stylish.txt'), 'utf-8');
+  expect(actual).toEqual(expected);
+});
+
+test('gendiff plain formatter for json', () => {
+  const actual = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
+  const expected = fs.readFileSync(getFixturePath('result-plain.txt'), 'utf-8');
+  expect(actual).toEqual(expected);
+});
+
+test('gendiff plain formatter for yml', () => {
+  const actual = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'), 'plain');
+  const expected = fs.readFileSync(getFixturePath('result-plain.txt'), 'utf-8');
   expect(actual).toEqual(expected);
 });
 
