e2f8dd3ecb23796eae3738c1842c7046e34f01b9
polina karna <polina.karna@gmail.com>
2024-04-15 22:54:33 +0300

    minor fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,9 +4,9 @@ import _ from 'lodash';
 const getIndent = (depth, sign = false) => ' '.repeat(4 * depth - (sign ? 2 : 0));
 
 // Checks if a tree node is object and recursively formats it into a string representation
-const formatNode = (value, depth) => {
+const formatNodeValue = (value, depth) => {
   if (_.isObject(value)) {
-    const formatEntry = ([key, val]) => `${getIndent(depth)}${key}: ${formatNode(val, depth + 1)}`;
+    const formatEntry = ([key, val]) => `${getIndent(depth)}${key}: ${formatNodeValue(val, depth + 1)}`;
     const nestedValues = Object.entries(value).map(formatEntry).join('\n');
     return `{\n${nestedValues}\n${getIndent(depth - 1)}}`;
   }
@@ -14,18 +14,19 @@ const formatNode = (value, depth) => {
 };
 
 // Return strings according to node types
-const createChange = (name, value, sign, depth) => `${getIndent(depth, true)}${sign} ${name}: ${formatNode(value, depth + 1)}`;
+const createChange = (name, value, sign, depth) => `${getIndent(depth, true)}${sign} ${name}: ${formatNodeValue(value, depth + 1)}`;
 const createAdded = (name, value, depth) => createChange(name, value, '+', depth);
 const createDeleted = (name, value, depth) => createChange(name, value, '-', depth);
 const createChanged = (name, from, to, depth) => `${createDeleted(name, from, depth)}\n${createAdded(name, to, depth)}`;
-const createUnchanged = (name, value, depth) => `${getIndent(depth)}${name}: ${formatNode(value, depth + 1)}`;
+const createUnchanged = (name, value, depth) => `${getIndent(depth)}${name}: ${formatNodeValue(value, depth + 1)}`;
 
 const makeStylish = (abstractTree) => {
   const buildStylishedTree = (tree, depth) => {
     const indent = getIndent(depth);
-    const stylishedTree = tree.map(({
-      type, name, value, from, to, children,
-    }) => {
+    const stylishedTree = tree.map((node) => {
+      const {
+        name, type, value, from, to, children,
+      } = node;
       switch (type) {
         case 'unchanged':
           return createUnchanged(name, value, depth);
